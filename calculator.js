26f657719fac502f9832dd672f182f18aa0c2cde
James S <98997059+jamesfsher@users.noreply.github.com>
2023-09-16 12:47:28 -0500

    added stylesheet

--- calculator.js
+++ calculator.js
@@ -13,3 +13,25 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, b, operator) {
+    if (operator == 'add') {
+        return add(a, b);
+    }
+    else if (operator == 'subtract') {
+        return subtract(a, b);
+    }
+    else if (operator == 'multiply') {
+        return multiply(a, b);
+    }
+    else if (operator == 'divide') {
+        return multiply(a, b);
+    }
+    else {
+        return "Operator not found";
+    }
+}
+
+let num1;
+let num2;
+let operator;
