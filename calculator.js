4a771c98082da98834b6604c78f6787271ef6f53
James S <98997059+jamesfsher@users.noreply.github.com>
2023-09-17 21:15:11 -0500

    JS file updated button logic

--- calculator.js
+++ calculator.js
@@ -32,10 +32,37 @@ function operate(a, b, operator) {
     }
 }
 
-function updateMain(num1, num2) {
 
+function updateMain(event) {
+    console.log(event);
 }
+
+const numbers = document.querySelectorAll('.btn.number');
+numbers.forEach(function (number) {
+    number.addEventListener('click', function () { 
+        updateMain(number);
+    });
+});
 
 let num1;
 let num2;
 let operator;
+let displayValue;
+
+
+
+// create event listeners on the numbers and operators to populate display value
+// Number has to be the first selection
+// multiple numbers can be added to each variable
+// Operator can be selected next
+// if a different operator is selected, initial option is overwritten
+// Next a string of numbers can be created again
+// if any operator is selected next, the numbers and operators previously selected are executed
+// the result of the previously operation should be moved to small display
+// the result of the past operation should then be stored as initial number
+// if user starts typing numbers, then stored past value is overwritten
+
+
+// Structure of passing variables with buttons
+// each button needs an event listener
+
